b78ac739625d85621b6c130c4effddcdd0c883f1
ilansarbac95 <ilansarbac@gmail.com>
2025-03-13 23:49:05 +0100

    Daily_challenge

--- week1/Day2/Day2_challenge_pc.py
+++ week1/Day2/Day2_challenge_pc.py
@@ -1,34 +1,23 @@
-#user_string = input("type a 10 char long string: ")
-#if len(user_string) < 10:
-    #print("string not long enough")
-#elif len(user_string) > 10:
-    #print("string is too long")
-#else:
-    #print("perfect string")
-
-#print(user_string[0])
-#print(user_string[1])
-
-#target_string = "HelloWorld"
-#constructed_string = ""
-
-#for char in target_string:
-   # constructed_string += char
-   # print(constructed_string)
-
 import random
 
-user_string = "HelloWorld" 
-user_list = list(user_string)
-random.shuffle(user_list)
-print(user_list)
-print("".join(user_list))
+user_string = input("type a 10 char long string: ")
 
-import random
+if len(user_string) < 10:
+    print("string not long enough")
+elif len(user_string) > 10:
+    print("string is too long")
+else:
+    print("perfect string")
+    print(user_string[0])
+    print(user_string[-1])  
 
-user_string = "HelloWorld"
-user_list = list(user_string)  
-random.shuffle(user_list)     
-jumbled_string = "".join(user_list)  
+    target_string = user_string 
+    constructed_string = ""
+    for char in target_string:
+       constructed_string += char
+       print(constructed_string)
 
-print(jumbled_string)
+    user_list = list(user_string)
+    random.shuffle(user_list)
+    jumbled_string = "".join(user_list)
+    print(jumbled_string)
