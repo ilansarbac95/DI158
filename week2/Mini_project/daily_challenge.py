42711cd1c54f8410ed67544b64540cd680f709e3
ilansarbac95 <ilansarbac@gmail.com>
2025-04-03 17:10:23 +0200

    Create daily_challenge.py

---
+++ week2/Mini_project/daily_challenge.py
@@ -0,0 +1,5 @@
+input_string = input(" Entrez une série de séquences de mots : ")
+words = input_string.split(",")  
+sorted_words = sorted(words)  
+output_string = ",".join(sorted_words)  
+print(output_string)
